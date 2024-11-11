b74c12e64d423406c2971abf08731e012e0a536d
Tấn Dự <tandu542003@gmail.com>
2024-11-11 13:50:56 +0700

    add pages admin

--- BikeApp/App.js
+++ BikeApp/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Screen01 from "./src/pages/Screen01";
 import Screen02 from "./src/pages/Screen02";
+import Admin from "./src/pages/Admin";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider } from "react-redux";
@@ -15,6 +16,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Screen01" component={Screen01} options={{ headerShown: false }} />
           <Stack.Screen name="Screen02" component={Screen02} options={{ headerShown: false }} />
+          <Stack.Screen name="Admin" component={Admin} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
